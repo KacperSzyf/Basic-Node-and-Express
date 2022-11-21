e6ad810dbd266ea916c289889ab8b8fcb8ad3e1b
KacperSzyf <45105722+KacperSzyf@users.noreply.github.com>
2022-11-21 20:16:34 +0000

    completed up to q7

--- myApp.js
+++ myApp.js
@@ -1,8 +1,35 @@
 let express = require('express');
 let app = express();
-console.log('Hello World')
 
+//logger
+app.use('/', (req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+})
 
+//static
+const static_path = __dirname + "/public"
+app.use('/public', express.static(static_path));
+
+//JSON
+app.get('/json', (req, res) => {
+  response = {"message" : "Hello json"};
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    response.message = response.message.toUpperCase();
+  }
+  res.json(response);
+})
+
+//index
+const file_path = __dirname + "/views/index.html"
+app.get('/', (req, res) => {
+  res.sendFile(file_path);
+});
+
+//server 'Hello Express'
+app.get("/hello", (req, res) => {
+  res.send("Hello Express");
+});
 
 
 
