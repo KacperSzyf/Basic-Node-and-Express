a0ead0704ce98cccab3f2e8fda96431d4ecb14fb
KacperSzyf <45105722+KacperSzyf@users.noreply.github.com>
2022-11-28 19:27:34 +0000

    Completed Basic Node and Express

--- myApp.js
+++ myApp.js
@@ -1,11 +1,12 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
 
 //logger
 app.use('/', (req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
-})
+}, bodyParser.urlencoded({extended: false}))
 
 //static
 const static_path = __dirname + "/public"
@@ -20,6 +21,16 @@ app.get('/json', (req, res) => {
   res.json(response);
 })
 
+//Time
+app.get('/now', (req, res, next) => {
+  //add time to req
+  req.time = new Date().toString();
+  next();
+  //chain middleware with handler
+}, (req, res) => {
+  res.json({"time": req.time});
+})
+
 //index
 const file_path = __dirname + "/views/index.html"
 app.get('/', (req, res) => {
@@ -30,6 +41,21 @@ app.get('/', (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("Hello Express");
 });
+
+//Echo chamber
+app.get('/:word/echo', (req, res) => {
+  res.send({"echo": req.params.word});
+})
+
+//Name
+app.route('/name').get((req, res) => {
+  res.json({"name": `${req.query.first} ${req.query.last}`});
+}).post((req,res) => {
+  console.log(req.body);
+  console.log({"name": `${req.body.first} ${req.body.last}`})
+  res.json({"name": `${req.body.first} ${req.body.last}`})
+})
+
 
 
 
